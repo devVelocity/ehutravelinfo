be8671a59a9e9323d33fd0be0e5501a7cdb62635
vlcty <88147999+devVelocity@users.noreply.github.com>
2023-01-29 01:16:25 +0000

    text

--- src/components/homeImgBanner.vue
+++ src/components/homeImgBanner.vue
@@ -1,13 +1,15 @@
 <template lang="">
     <article id="homeImageBanner" class="w-full sm:h-[500px] h-[250px] flex items-center justify-center overflow-hidden relative">
-        <section class="h-full w-full">
-            <span class="bg-black absolute w-full h-full bg-opacity-50 z-20"></span>
-            <section class="absolute w-full h-full flex items-center justify-center">
-                <h1></h1>
+        <section class="h-full w-full flex items-center justify-center">
+            <span class="bg-black absolute w-full h-full bg-opacity-70 z-20"></span>
+            <section class="absolute w-[45%] h-full flex items-center justify-center z-30 flex-col gap-4">
+                <h1 class="text-white text-4xl font-bold">Edge Hill Travel Information</h1>
+                <span class="w-full h-[1px] inline-block bg-white rounded-md"></span>
+                <h2 class="text-white text-lg font-extralight">Train / Bus Information</h2>
             </section>
-            <img src="@/assets/20221125_112618.jpg" class="object-cover w-full h-full absolute bannerImg" style="opacity: 1" id="image1"/>
-            <img src="@/assets/20230106_073903.jpg" class="object-cover w-full h-full absolute bannerImg" style="opacity: 0" id="image2"/>
-            <img src="@/assets/150ormskirk.jpg" class="object-cover w-full h-full absolute bannerImg" style="opacity: 0" id="image3"/>
+            <img src="@/assets/20221125_112618.jpg" class="object-cover w-full h-full absolute bannerImg blur-sm" style="opacity: 1" id="image1"/>
+            <img src="@/assets/20230106_073903.jpg" class="object-cover w-full h-full absolute bannerImg blur-sm" style="opacity: 0" id="image2"/>
+            <img src="@/assets/150ormskirk.jpg" class="object-cover w-full h-full absolute bannerImg blur-sm" style="opacity: 0" id="image3"/>
         </section>
     </article>
 </template>
